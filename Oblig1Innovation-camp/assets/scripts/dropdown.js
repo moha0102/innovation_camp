9742cc35dd456aeb479a16815350c70077f4ebb9
moha0102 <mohammedak0102@gmail.com>
2020-10-21 19:10:41 +0200

    Update dropdown.js

--- Oblig1Innovation-camp/assets/scripts/dropdown.js
+++ Oblig1Innovation-camp/assets/scripts/dropdown.js
@@ -2,26 +2,26 @@ const menu = document.getElementById('nav-menu');
 const nav = document.getElementById('nav');
 const bars = document.getElementById('bars');
 
-menu.style.display = "none";
+if (window.outerWidth > 760) {
+  menu.style.display = 'flex';
+  document.documentElement.style.overflow = 'visible';
+  nav.style.backgroundColor = '#F6F7EB';
+  bars.style.color = 'black';
+} else {
+  menu.style.display = "none";
+}
+
 
 window.onresize = function() {
-  menu.style.display = "none";
-  document.documentElement.style.overflow = 'hidden';
   if (window.outerWidth > 760) {
-    menu.style.display = "flex";
+    menu.style.display = 'flex';
     document.documentElement.style.overflow = 'visible';
     nav.style.backgroundColor = '#F6F7EB';
     bars.style.color = 'black';
   } else {
-    if (window.outerWidth <= 760 && menu.style.display === 'flex') {
-      nav.style.backgroundColor = '#212A3B';
-      document.documentElement.style.overflow = 'hidden';
-      bars.style.color = 'white';
-    } else {
-      nav.style.backgroundColor = '#F6F7EB';
-      bars.style.color = 'black';
-
-    }
+    menu.style.display = "none";
+    nav.style.backgroundColor = '#F6F7EB';
+    bars.style.color = 'black';
   }
 }
 
