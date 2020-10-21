3b132cb5cc9ae495d16570578d6ea6d9b9e5f76c
moha0102 <mohammedak0102@gmail.com>
2020-10-21 18:58:44 +0200

    Menu

--- Oblig1Innovation-camp/assets/scripts/dropdown.js
+++ Oblig1Innovation-camp/assets/scripts/dropdown.js
@@ -3,6 +3,27 @@ const nav = document.getElementById('nav');
 const bars = document.getElementById('bars');
 
 
+window.onresize = function() {
+  menu.style.display = "none";
+  if (window.outerWidth > 760) {
+    menu.style.display = "flex";
+    document.documentElement.style.overflow = 'visible';
+    nav.style.backgroundColor = '#F6F7EB';
+    bars.style.color = 'black';
+  } else {
+    if (window.outerWidth <= 760 && menu.style.display === 'flex') {
+      nav.style.backgroundColor = '#212A3B';
+      document.documentElement.style.overflow = 'hidden';
+      bars.style.color = 'white';
+    } else {
+      nav.style.backgroundColor = '#F6F7EB';
+      bars.style.color = 'black';
+
+    }
+  }
+}
+
+
 bars.onclick = function() {
   if (menu.style.display === 'none') {
     menu.style.display = "flex";
