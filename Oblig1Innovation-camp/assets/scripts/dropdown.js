a836cc1f62bbd88e4cbe4f63792340a5a001bee9
moha0102 <mohammedak0102@gmail.com>
2020-10-21 18:16:29 +0200

    Responsiveness

--- Oblig1Innovation-camp/assets/scripts/dropdown.js
+++ Oblig1Innovation-camp/assets/scripts/dropdown.js
@@ -1,8 +1,6 @@
 const menu = document.getElementById('nav-menu');
 const nav = document.getElementById('nav');
 const bars = document.getElementById('bars');
-const logoName = document.getElementsByClassName('logo-name')[0];
-
 
 
 bars.onclick = function() {
@@ -11,7 +9,6 @@ bars.onclick = function() {
     document.documentElement.style.overflow = 'hidden';
     nav.style.backgroundColor = '#212A3B';
     bars.style.color = 'white';
-    logoName.style.color = 'white'; 
    
    
   } else {
@@ -19,6 +16,5 @@ bars.onclick = function() {
     document.documentElement.style.overflow = 'visible';
     nav.style.backgroundColor = '#F6F7EB';
     bars.style.color = 'black';
-    logoName.style.color = 'black';
     }
   }
